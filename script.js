cf9074d8192388ebc5258b24ec0c1751e1532e7c
Kenyansa <kenyansajustine@gmail.com>
2023-07-29 22:55:17 +0300

    add click event to all add to cart buttons

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let addToCartButtons = document.getElementById("btn-primary");
 let cartContainer = document.getElementsByTagName("tbody")[0];
 let quantityFields = document.getElementsByClassName("num");
 let delete_buttons = document.getElementsByClassName("uk-button-danger");
+
+//get all the add-to-cart buttons
+for(let i=0;
+    i<addToCartButtons.clientHeight;
+    i++
+    ){
+        addToCartButtons[i].addEventListener('click', addToCart)
+}
