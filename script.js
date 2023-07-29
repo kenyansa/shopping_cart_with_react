2968c35902f49e16d63f99f767e8e449d08377af
Kenyansa <kenyansajustine@gmail.com>
2023-07-29 22:47:20 +0300

    declarations of inputs

---
+++ script.js
@@ -0,0 +1,4 @@
+let addToCartButtons = document.getElementById("btn-primary");
+let cartContainer = document.getElementsByTagName("tbody")[0];
+let quantityFields = document.getElementsByClassName("num");
+let delete_buttons = document.getElementsByClassName("uk-button-danger");
