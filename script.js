560dd9680f0bfa65961a4f1a0088f4fa02b3888c
Kenyansa <kenyansajustine@gmail.com>
2023-07-29 23:08:31 +0300

    function for adding items to our cart and appending the child

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ for(let i=0;
     ){
         addToCartButtons[i].addEventListener('click', addToCart)
 }
+//function for adding items to our cart
+function addToCart(e){
+    let itemContainer = document.createElement("tr");
+    let btn = e.target;
+    let btnGrandParent = btn.parentElement.parentElement;
+    let btnParent = btn.parentElement;
+    let itemImage = btnGrandParent.children[0].src;
+    let itemName = btnParent.children[0].innerText;
+    let itemPrice = btnParent.children[1].innerText;
+
+    itemContainer.innerHTML = `
+    <td><input class="uk-checkbox" type="checkbox"></td>
+    <td><img class="uk-preserve-width uk-border-circle" src=${itemImage} width="40" alt=""></td>
+    <td class="uk-table-link">
+        <h3 class = "item-name">${itemName}</h3>
+    </td>
+    <td class="uk-text-truncate item-price"><h3>${itemPrice}</h3></td>
+    <td><input type = 'number' class = 'num' value = '1'></td>
+    <td class="uk-text-truncate total-price"><h3>${itemPrice}</h3></td>
+    <td><button class="uk-button uk-button-danger" type="button">Remove</button></td>
+    `
+    cartContainer.append(itemContainer);
+}
